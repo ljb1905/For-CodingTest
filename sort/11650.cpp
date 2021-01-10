be311e9412c478af4df414922be9d2ff89501f63
LeeJongbin <ljb1905@naver.com>
2021-01-10 17:24:21 +0900

    ### 11650 struct operator

---
+++ sort/11650.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+struct s {
+	int x, y;
+	s(){}
+	s(int x,int y)
+		:x(x),y(y)
+	{}
+	bool operator < (const s& o)const {
+		if (x == o.x)
+			return y < o.y;
+		return x < o.x;
+	}
+}arr[100001];
+
+int main()
+{
+	int n; scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+	return 0;
+}
