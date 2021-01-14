8d9b1fb3f596a446b8c4207996cf3668d29e280b
LeeJongbin <ljb1905@naver.com>
2021-01-14 13:43:43 +0900

    1904

---
+++ DP/1904.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int dp[1000001];
+
+int main()
+{
+	int n; scanf("%d", &n);
+	dp[0] = 1, dp[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2])%15746;
+	}
+	printf("%d\n", dp[n]);
+	return 0;
+}
