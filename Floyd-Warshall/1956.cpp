1499391d33d73c58b6d2611d1468f66b230c8ce7
LeeJongbin <ljb1905@naver.com>
2021-02-18 22:01:46 +0900

    ### 1956 플로이드, 사이클 찾기

--- Floyd-Warshall/1956.cpp
+++ Floyd-Warshall/1956.cpp
@@ -1,44 +1,32 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
 
-int main ()
-{
-	int dis[401][401];
-	int n,m;
-	scanf("%d %d",&n,&m);
-	for(int i=1;i<=n;i++)
-	{
-		for(int j=1;j<=n;j++)
-		{
-			dis[i][j]=987654321;
+int dis[401][401];
+
+int main() {
+	int n, m; scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			dis[i][j] = 987654321;
 		}
 	}
-	while(m--)
-	{
-		int a,b,c;
-		scanf("%d %d %d",&a,&b,&c);
-		dis[a][b]=c;
+	for (int i = 0; i < m; i++) {
+		int a, b, c; scanf("%d %d %d", &a, &b, &c);
+		dis[a][b] = c;
 	}
-	for(int k=1;k<=n;k++)
-	{
-		for(int i=1;i<=n;i++)
-		{
-			for(int j=1;j<=n;j++)
-			{
-				if(dis[i][j]>dis[i][k]+dis[k][j]) dis[i][j]=dis[i][k]+dis[k][j];
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
 			}
 		}
 	}
-	int ans=987654321;
-	bool sw=false;
-	for(int i=1;i<=n;i++)
-	{
-		if(ans>dis[i][i] && dis[i][i]!=987654321)
-		{
-			sw=true;
-			ans=dis[i][i];
-		}
-	}
-	if(sw) printf("%d\n",ans);
-	else printf("-1\n");	
+	int ans = 987654321;
+	for (int i = 1; i <= n; i++)
+		ans = min(ans, dis[i][i]);
+	if (ans == 987654321)
+		ans = -1;
+	printf("%d\n", ans);
 	return 0;
 }
