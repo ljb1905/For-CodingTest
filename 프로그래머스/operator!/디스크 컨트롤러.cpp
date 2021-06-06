ef92526fb3477540c31543554054f14df7b67651
JB <ljb1905@naver.com>
2021-06-06 22:25:02 +0900

    ### compare함수 & 구조체 오퍼레이터

--- 프로그래머스/operator!/디스크 컨트롤러.cpp
+++ 프로그래머스/operator!/디스크 컨트롤러.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,30 +12,50 @@ struct str{
         st(st),ed(ed)
         {}
     bool operator <(const str &o)const{
-        if(ed==o.ed){
-            return st<o.st;
+        if(st==o.st){
+            return ed<o.ed;
         }
-        return ed<o.ed;
+        return st<o.st;
     }
 };
-struct compare2{
+struct compare{
     bool operator()(str &A, str &B){
-        if(A.st==B.st){
-            return A.ed<B.ed;
+        if(A.ed==B.ed){
+            return A.st>B.st;
         }
-        return A.st<B.st;
+        return A.ed>B.ed;
     }
 };
 
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
     vector <str> v;
+    priority_queue <str,vector<str>,compare> pq;
     for(int i=0;i<jobs.size();i++){
         v.push_back(str(jobs[i][0],jobs[i][1]));
     }
     sort(v.begin(),v.end());
-    for(int i=0;i<jobs.size();i++){
-        printf("%d %d\n",v[i].st,v[i].ed);
+    int cnt = 0;
+    int now = 0;
+    int n = v.size();
+    int idx = 0;
+    while(cnt < n){
+        bool flag = false;
+        for(;idx<n;idx++){
+            if(now >= v[idx].st){
+                pq.push(v[idx]);
+            }
+            else break;
+        }
+        if(!pq.empty()){
+            answer += now - pq.top().st + pq.top().ed;
+            now += pq.top().ed;
+            pq.pop();
+            cnt++;
+            flag = true;
+        }
+        if(!flag) now++;
     }
+    answer = answer/n;
     return answer;
 }
