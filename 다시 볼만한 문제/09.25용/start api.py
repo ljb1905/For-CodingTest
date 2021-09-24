441e5648623a10e74cedee6c908d99d627e96460
JB <ljb1905@naver.com>
2021-09-25 00:40:41 +0900

    수정

--- 다시 볼만한 문제/09.25용/start api.py
+++ 다시 볼만한 문제/09.25용/start api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users/start'
-headers = {'X-Auth-Token': 'f366db55a4dd0893c9bd15a5ce46c858', 'Content-Type': 'application/json'}
+url = 'url/start'
+headers = {'X-Auth-Token': 'val', 'Content-Type': 'application/json'}
 parameter = {'problem': 1}
 r = requests.post(url, headers=headers, params=parameter)
