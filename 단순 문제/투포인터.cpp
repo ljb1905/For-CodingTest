9f62883a6adda157377c2cc0aa001a856ffad00c
Jongbin Lee <ljb1905@naver.com>
2022-03-14 23:29:40 +0900

    투포인터 수정중

---
+++ 단순 문제/투포인터.cpp
@@ -0,0 +1,53 @@
+//프로그래머스 보석쇼핑
+
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    vector<int> answer;
+    map<string,int> mapp;
+    map<string,int> nowmapp;
+    int cnt = 1;
+    for(int i=0;i<gems.size();i++){
+        if(mapp.find(gems[i])==mapp.end()){
+            mapp.insert(make_pair(gems[i],cnt++));
+        }
+    }
+    cnt--;
+    int l = 0;
+    int r = 0;
+    int ansl = 0;
+    int ansr = 0;
+    int ansmin = 987654321;
+    int nowcnt = 1;
+    nowmapp[gems[l]]++;
+    int n = gems.size();
+    while(1){
+        if(l==n || r==n) break;
+        if(nowcnt<cnt){
+            r++;
+            if(nowmapp[gems[r]]==0){
+                nowcnt++;
+            }
+            nowmapp[gems[r]]++;
+        }
+        else{
+            if(nowmapp[gems[l]]==1){
+                if(ansmin > r-l){
+                    ansl = l;
+                    ansr = r;
+                    ansmin = r-l;
+                }
+                nowcnt--;
+            }
+            nowmapp[gems[l]]--;
+            l++;          
+        }
+    }
+    answer.push_back(ansl+1);
+    answer.push_back(ansr+1);
+    return answer;
+}
